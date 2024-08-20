715c84dbc4b21408a6713e4a85fbcb74f9ecb6ba
facuhenest <162722573+facuhenest@users.noreply.github.com>
2024-08-20 17:22:35 -0300

    Update tp-2-facuhenest.py
    update-2

--- tp-2-facuhenest.py
+++ tp-2-facuhenest.py
@@ -1,38 +1,42 @@
 #ejercicio 1
-first_name = "AdA"
-last_name = "LoVeLAce"
 
-print(first_name.lower(), " ", last_name.lower())
-print(first_name.title(), " ", last_name.title())
-print(first_name.upper(), " ", last_name.upper())
-print("\t" + first_name.lower(), " ", last_name.lower())
+def ada():
+  first_name = "AdA"
+  last_name = "LoVeLAce"
+
+  print(first_name.lower(), " ", last_name.lower())
+  print(first_name.title(), " ", last_name.title())
+  print(first_name.upper(), " ", last_name.upper())
+  print("\t" + first_name.lower(), " ", last_name.lower())
 
 #ejercicio 2
 
-X="Barbados"
-Y="Bangladesh"
-print("The result of ", X, " comes first in the dictionary than", Y, "is", X<Y)
-print("The result of ", Y, " comes first in the dictionary than", X, "is", Y<X)
+def earth():
+  X="Barbados"
+  Y="Bangladesh"
+  print("The result of ", X, " comes first in the dictionary than", Y, "is", X<Y)
+  print("The result of ", Y, " comes first in the dictionary than", X, "is", Y<X)
 
 #ejercicio 3
-gasto= 23.75
-dinero= 100
 
-print("Ingresar gasto:")
-print(gasto)
-print("Dinero recibido")
-print(dinero)
-
-print(" ")
-print("Vuelto")
-print(" ")
-
-print("Pesos:")
-gasto2=float(gasto)
-dinero2=float(dinero)
-vuelto= dinero2-gasto2
-print(vuelto)
-print("Centavos:")
-centavos=int(((dinero2-gasto2)-int(dinero2-gasto2))*100)
-
-print(centavos)
+def change():
+  gasto= 23.75
+  dinero= 100
+  
+  print("Ingresar gasto:")
+  print(gasto)
+  print("Dinero recibido")
+  print(dinero)
+  
+  print(" ")
+  print("Vuelto")
+  print(" ")
+  
+  print("Pesos:")
+  gasto2=float(gasto)
+  dinero2=float(dinero)
+  vuelto= dinero2-gasto2
+  print(vuelto)
+  print("Centavos:")
+  centavos=int(((dinero2-gasto2)-int(dinero2-gasto2))*100)
+  print(centavos)
