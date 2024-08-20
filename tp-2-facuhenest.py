c5aea1d184cf17ae560ddfa0a2d6bc447d71f8bc
facuhenest <162722573+facuhenest@users.noreply.github.com>
2024-08-20 15:36:11 -0300

    Add files via upload

--- tp-2-facuhenest.py
+++ tp-2-facuhenest.py
@@ -1,3 +1,4 @@
+#ejercicio 1
 first_name = "AdA"
 last_name = "LoVeLAce"
 
@@ -6,3 +7,28 @@ print(first_name.title(), " ", last_name.title())
 print(first_name.upper(), " ", last_name.upper())
 print("\t" + first_name.lower(), " ", last_name.lower())
 
+#ejercicio 2
+
+X="Barbados"
+Y="Bangladesh"
+print("The result of ", X, " comes first in the dictionary than", Y, "is", X>Y)
+print("The result of ", Y, " comes first in the dictionary than", X, "is", Y>X)
+
+#ejercicio 3
+
+gasto=input("Ingresar gasto:\n")
+
+dinero=input("Dinero recibido \n")
+
+print(" ")
+print("Vuelto")
+print(" ")
+print("Pesos:")
+gasto2=float(gasto)
+dinero2=float(dinero)
+vuelto= dinero2-gasto2
+print(vuelto)
+print("Centavos:")
+centavos=int(((dinero2-gasto2)-int(dinero2-gasto2))*100)
+
+print(centavos)
